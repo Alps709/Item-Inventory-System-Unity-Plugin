554935a5966f9bd4c3d5ba2436c142c2207710e0
Anton Le Prevost-Smith <alps709@hotmail.com>
2020-10-16 12:17:58 +1300

    Added basic interaction which items will inherit from

--- Item-Inventory-System/Assets/Scripts/MyItem.cs
+++ Item-Inventory-System/Assets/Scripts/MyItem.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
     //Id used to identify the item
     private uint ID;
     private new string name;
     private string descripton;
+    
+    //The stats of the item
+    private Dictionary<string, int> stats;
+
+    public Transform ItemTransform;
 
     //Object model (for when this item can be dropped in the world)
     protected Mesh mesh;
 
-    //The stats of the item
-    private Dictionary<string, int> stats;
+    
 
     public Item(uint id, string name, string descripton, Component[] components, Dictionary<string, int> stats)
     {
@@ -21,5 +26,10 @@ public class Item : ScriptableObject
         this.name = name;
         this.descripton = descripton;
         this.stats = stats;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
     }
 }
---
+++ Item-Inventory-System/Assets/Scripts/MyPlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerController : MonoBehaviour
+{
+    public Camera Cam;
+
+    public Interactable FocusedItem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray HitRay = Cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit RayCastHit;
+        Interactable HitInteractable = null;
+        
+        if (Physics.Raycast(HitRay, out RayCastHit, 100))
+        {
+            HitInteractable = RayCastHit.collider.GetComponent<Interactable>();
+        }
+        
+        if (HitInteractable != null)
+        {
+            SetFocus(HitInteractable);
+        }
+        else
+        {
+            RemoveFocus();
+        }
+
+        if (Input.GetMouseButtonDown(1) && FocusedItem != null)
+        {
+            //Pickup Item
+        }
+    }
+
+    void SetFocus(Interactable NewFocus)
+    {
+        if (NewFocus != FocusedItem)
+        {
+            //Defocus previous item
+            if (FocusedItem != null)
+            {
+                FocusedItem.OnDefocused();
+            }
+            
+            //Set new focused item
+            FocusedItem = NewFocus;
+            FocusedItem.OnFocused(transform);
+        }
+    }
+
+    void RemoveFocus()
+    {
+        if (FocusedItem != null)
+        {
+            FocusedItem.OnDefocused();
+        }
+        FocusedItem = null;
+    }
+}
