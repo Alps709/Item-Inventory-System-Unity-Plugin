edd2b5d15c2eae3cd967fe76c7c03d5b29d8f563
Anton Le Prevost-Smith <alps709@hotmail.com>
2020-10-16 10:55:26 +1300

    Added a player first person camera

---
+++ Item-Inventory-System/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float MouseSensitivity = 100f;
+    public Transform PlayerBody;
+    public float CameraAngle = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        CameraAngle -= MouseY;
+        CameraAngle = Mathf.Clamp(CameraAngle, -90.0f, 90.0f);
+        transform.localRotation = Quaternion.Euler(CameraAngle, 0.0f, 0.0f);
+        PlayerBody.Rotate(Vector3.up, MouseX);
+    }
+}
